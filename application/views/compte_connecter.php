59640a600d6b97874609416ca4e57a28df01e279
YACOUBI-Abdelhakime <hakim.199911@gmail.com>
2021-11-22 21:50:30 +0100

    add-session-tst1

---
+++ application/views/compte_connecter.php
@@ -0,0 +1,19 @@
+
+
+
+
+<div class="nav-space"></div>
+<section id="actualites" class="section-with-bg">
+<div class="section-header">
+    <h2>Connexion</h2>
+</div>
+    <?php echo validation_errors(); ?>
+    <?php echo form_open('compte/connecter'); ?>
+    <label>Saisissez vos identifiants ici :</label><br>
+    <input type="text" name="pseudo" required />
+    <input type="text" name="mdp" required/>
+    <input type="submit" value="Connexion"/>
+    </form>
+
+</section>
+
